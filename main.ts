0457251120406569c19566b84c635e263cd79d31
Artem Purundzhan <artempa1607@gmail.com>
2024-06-25 10:30:49 +0100

    feat: add auth flow

--- main.ts
+++ main.ts
@@ -1,134 +1,283 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
+import {
+  App,
+  Editor,
+  MarkdownView,
+  Modal,
+  Notice,
+  Plugin,
+  PluginSettingTab,
+  RequestUrlParam,
+  Setting,
+  WorkspaceLeaf,
+  request,
+} from 'obsidian';
+import { EmailView, VIEW_TYPE_EMAIL } from './email-view';
+import { OTPView, VIEW_TYPE_OTP } from './otp-view';
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+  mySetting: string;
+  jwtToken: string | null;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+  mySetting: 'default',
+  jwtToken: null,
+};
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  settings: MyPluginSettings;
+  private authRequestId: string | null = null;
+  private jwtToken: string | null = null; // Declare jwtToken here
 
-	async onload() {
-		await this.loadSettings();
+  async openEmailView() {
+    const leaf = this.app.workspace.getLeaf(true);
+    await leaf.setViewState({
+      type: VIEW_TYPE_EMAIL,
+      active: true,
+    });
+    this.app.workspace.revealLeaf(leaf);
+  }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+  async openOTPView() {
+    const leaf = this.app.workspace.getLeaf(true);
+    await leaf.setViewState({
+      type: VIEW_TYPE_OTP,
+      active: true,
+    });
+    this.app.workspace.revealLeaf(leaf);
+  }
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+  async handleEmailSubmit(email: string) {
+    // Make API request to send OTP to the email
+    const url = `https://98d0-81-133-73-3.ngrok-free.app/api/auth/login`;
+    const options: RequestUrlParam = {
+      url: url,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email }),
+    };
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    try {
+      const response = await request(options);
+      const res = JSON.parse(response);
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+      if (res.error) {
+        throw new Error(res.message);
+      }
+      this.authRequestId = res.auth_request_id;
+      new Notice('OTP sent to your email.');
+      this.app.workspace.detachLeavesOfType(VIEW_TYPE_EMAIL);
+      this.openOTPView();
+    } catch (err) {
+      new Notice(err.message || 'Unknown error');
+    }
+  }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+  async handleOTPSubmit(otp: string) {
+    // Handle OTP submission logic
+    if (!this.authRequestId) {
+      new Notice('No auth request ID found. Please start the process again.');
+      return;
+    }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    const url = `https://98d0-81-133-73-3.ngrok-free.app/api/auth/verify-code`;
+    const options: RequestUrlParam = {
+      url: url,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ auth_request_id: this.authRequestId, code: otp }),
+    };
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    try {
+      const response = await request(options);
+      const res = JSON.parse(response);
 
-	onunload() {
+      if (res.error) {
+        throw new Error(res.message);
+      }
 
-	}
+      this.jwtToken = res.jwt_token;
+      new Notice(`${this.jwtToken}`, 3000);
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+      this.settings.jwtToken = res.jwt_token; // Store the JWT token in settings
+      await this.saveSettings(); // Save settings to persist jwtToken
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+      this.app.workspace.detachLeavesOfType(VIEW_TYPE_OTP);
+      new Notice('Login successful!');
+    } catch (err) {
+      new Notice(err.message || 'Unknown error');
+    }
+  }
+
+  async onload() {
+    await this.loadSettings();
+    new Notice('Plugin Re-Loaded! testing');
+    // This creates an icon in the left ribbon.
+    const ribbonIconEl = this.addRibbonIcon(
+      'dice',
+      'Sample Plugin',
+      (evt: MouseEvent) => {
+        // Called when the user clicks the icon.
+        new Notice('This is a notice!');
+      }
+    );
+    // Perform additional things with the ribbon
+    ribbonIconEl.addClass('my-plugin-ribbon-class');
+
+    // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+    const statusBarItemEl = this.addStatusBarItem();
+    statusBarItemEl.setText('Status Bar Text');
+
+    this.registerView(
+      VIEW_TYPE_EMAIL,
+      (leaf: WorkspaceLeaf) =>
+        new EmailView(leaf, (email) => this.handleEmailSubmit(email))
+    );
+
+    this.registerView(
+      VIEW_TYPE_OTP,
+      (leaf: WorkspaceLeaf) =>
+        new OTPView(leaf, (otp) => this.handleOTPSubmit(otp))
+    );
+
+    this.addCommand({
+      id: 'open-email-view',
+      name: 'Enter Email',
+      callback: () => this.openEmailView(),
+    });
+
+    // This adds a simple command that can be triggered anywhere
+    this.addCommand({
+      id: 'custom-command',
+      name: 'Custom command',
+      callback: async () => {
+        try {
+          // Fetch data from a public API (e.g., a joke API)
+          const response = await fetch(
+            'https://official-joke-api.appspot.com/random_joke'
+          );
+          const data = await response.json();
+
+          const newFile = await this.app.vault.create(
+            `${data.setup}.md`,
+            data.punchline
+          );
+
+          const leaf = this.app.workspace.getLeaf(true);
+          await leaf.openFile(newFile);
+        } catch (error) {
+          new Notice('Error fetching data. Check console for details.');
+        }
+      },
+    });
+
+    // This adds a simple command that can be triggered anywhere
+    this.addCommand({
+      id: 'open-sample-modal-simple',
+      name: 'Open sample modal (simple)',
+      callback: () => {
+        new SampleModal(this.app).open();
+      },
+    });
+    // This adds an editor command that can perform some operation on the current editor instance
+    this.addCommand({
+      id: 'sample-editor-command',
+      name: 'Sample editor command',
+      editorCallback: (editor: Editor, view: MarkdownView) => {
+        console.log(editor.getSelection());
+        editor.replaceSelection('Sample Editor Command');
+      },
+    });
+    // This adds a complex command that can check whether the current state of the app allows execution of the command
+    this.addCommand({
+      id: 'open-sample-modal-complex',
+      name: 'Open sample modal (complex)',
+      checkCallback: (checking: boolean) => {
+        // Conditions to check
+        const markdownView =
+          this.app.workspace.getActiveViewOfType(MarkdownView);
+        if (markdownView) {
+          // If checking is true, we're simply "checking" if the command can be run.
+          // If checking is false, then we want to actually perform the operation.
+          if (!checking) {
+            new SampleModal(this.app).open();
+          }
+
+          // This command will only show up in Command Palette when the check function returns true
+          return true;
+        }
+      },
+    });
+
+    // This adds a settings tab so the user can configure various aspects of the plugin
+    this.addSettingTab(new SampleSettingTab(this.app, this));
+
+    // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+    // Using this function will automatically remove the event listener when this plugin is disabled.
+    this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+      console.log('click', evt);
+    });
+
+    // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+    this.registerInterval(
+      window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000)
+    );
+  }
+
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
 class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+  constructor(app: App) {
+    super(app);
+  }
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+  onOpen() {
+    const { contentEl } = this;
+    contentEl.setText('Woah!');
+  }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+  onClose() {
+    const { contentEl } = this;
+    contentEl.empty();
+  }
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+  plugin: MyPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: MyPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		const {containerEl} = this;
+  display(): void {
+    const { containerEl } = this;
 
-		containerEl.empty();
+    containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(containerEl)
+      .setName('Setting #1')
+      .setDesc("It's a secret")
+      .addText((text) =>
+        text
+          .setPlaceholder('Enter your secret')
+          .setValue(this.plugin.settings.mySetting)
+          .onChange(async (value) => {
+            this.plugin.settings.mySetting = value;
+            await this.plugin.saveSettings();
+          })
+      );
+  }
 }
